6682e458b81dcfde2753227a443b5f4eec289965
Williamjondahl <williamj@uia.no>
2023-11-22 14:36:43 +0100

    fiksa aaron js

--- js/figures.js
+++ js/figures.js
@@ -49,6 +49,37 @@ function drawTriangle(startposx, startposy, line1x, line1y, line2x, line2y, line
     ctx.stroke();
 }
 
+function drawStar(cx, cy, spikes, outerRadius, innerRadius, color) {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    let rot = Math.PI / 2 * 3;
+    let x = cx;
+    let y = cy;
+    let step = Math.PI / spikes;
+
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - outerRadius);
+
+    for (let i = 0; i < spikes; i++) {
+      x = cx + Math.cos(rot) * outerRadius;
+      y = cy + Math.sin(rot) * outerRadius;
+      ctx.lineTo(x, y);
+      rot += step;
+
+      x = cx + Math.cos(rot) * innerRadius;
+      y = cy + Math.sin(rot) * innerRadius;
+      ctx.lineTo(x, y);
+      rot += step;
+    }
+
+    ctx.lineTo(cx, cy - outerRadius);
+    ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+
 function rectangleSVG(x, y, width, height, mode, color) {   
     const svg = document.getElementById("mySVG");
     const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
@@ -99,4 +130,13 @@ function circleSVG(x, y, radius, mode, color) {
         circle.setAttribute("fill", color);
         svg.appendChild(circle);  
     }
+}
+
+function starSVG(startposx, startposy, pos2x, pos2y, pos3x, pos3y, pos4x, pos4y, pos5x, pos5y, color) {
+    const svg = document.getElementById("mySVG");
+    const star = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+
+    star.setAttribute("points", startposx + "," + startposy + " " + pos2x + "," + pos2y + " " + pos3x + "," + pos3y + " " + pos4x + "," + pos4y + " " + pos5x + "," + pos5y);
+    star.setAttribute("fill", color);
+    svg.appendChild(star);
 }
