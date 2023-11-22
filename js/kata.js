d3f32a24476938652483bcd1b0167eea103281ea
Williamjondahl <williamj@uia.no>
2023-11-22 15:03:24 +0100

    Fiksa kata.js

---
+++ js/kata.js
@@ -0,0 +1,5 @@
+drawRectangle(40, 40, 60, 60, "fill", "navy");
+drawStar(70, 70, 5, 25, 10, "gold");
+
+rectangleSVG(15, 17.5, 70, 70, "fill", "navy");
+starSVG(50, 20, 70, 80, 20, 43, 80, 43, 30, 80, "gold")
