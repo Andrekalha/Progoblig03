a25388e9a10ab290928595fb9d99d3b4dea1c147
Williamjondahl <williamj@uia.no>
2023-11-22 14:48:15 +0100

    justert andre . js

--- js/andre.js
+++ js/andre.js
@@ -1,10 +1,10 @@
-drawRectangle(80, 20, 100, 120, "fill", "Red");
-drawRectangle(80, 120, 40, 50, "fill", "Red");
-drawRectangle(140, 120, 40, 50, "fill", "Red");
-drawRectangle(60, 35, 50, 80, "fill", "Red");
-drawRectangle(60, 35, 20, 80, "outline", "Black");
-drawCircle(170, 60, 25, "fill", "grey")
-drawCircle(170, 60, 25, "outline", "black")
+drawRectangle(80, 120, 100, 120, "fill", "Red");
+drawRectangle(80, 220, 40, 50, "fill", "Red");
+drawRectangle(140, 220, 40, 50, "fill", "Red");
+drawRectangle(60, 155, 50, 80, "fill", "Red");
+drawRectangle(60, 155, 20, 80, "outline", "Black");
+drawCircle(170, 160, 25, "fill", "grey")
+drawCircle(170, 160, 25, "outline", "black")
 
 rectangleSVG(80, 120, 100, 120, "fill", "Red");
 rectangleSVG(80, 220, 40, 50, "fill", "Red");
